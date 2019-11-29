ac8cc13eb81f67cd75f1f50a16d7fde6b65c1c4c
Juanjo Salvador <juanjosalvador@netc.eu>
2019-11-29 22:06:44 +0100

    hitbox fine tunning

--- horchata/collision.lua
+++ horchata/collision.lua
@@ -2,14 +2,19 @@ local Collision = {}
 
 -- ob1: objeto que impacta
 -- ob2: objeto impactado
+-- hbx: ajuste hitbox X
+-- hby: ajuste hitbox y
 -- devuelve las coordenadas donde ha sido impactado
 
-function Collision:detect(ob1, ob2)
+function Collision:detect(ob1, ob2, hbx, hby)
     local col = {}
 
+    local hbx = hbx or 0
+    local hby = hby or 0
+
     if ob1 ~= nil then
-        if ob1.y <= ob2.y + ob2.h and ob1.y + ob1.h >= ob2.y then
-            if ob1.x + ob1.w >= ob2.x and ob1.x + ob1.w <= ob2.x + ob2.w then
+        if ob1.y <= ob2.y + (ob2.h - hby) and ob1.y + ob1.h >= ob2.y + hby then
+            if ob1.x + ob1.w >= ob2.x + hbx and ob1.x <= ob2.x + (ob2.w - hbx) then
                 col.enter = true
             end
         end
