a56e8fbd17c63ce08da80bc007f5f77041bbe0d6
Juanjo Salvador <juanjosalvador@netc.eu>
2020-04-18 18:11:59 +0200

    Updated docs strings

--- horchata/menu.lua
+++ horchata/menu.lua
@@ -1,3 +1,9 @@
+--- Creates beautiful menus for your Love2D games
+-- @module horchata.menu
+-- @author Juanjo Salvador
+-- @copyright 2020
+-- @license MIT
+
 local Menu = {}
 
 local Color = require "lib.horchata.color"
@@ -5,9 +11,9 @@ local Color = require "lib.horchata.color"
 local entries = {}
 local screenwidth, screenheight, percent_x, percent_y, color
 
---- Creates a new Menu
--- @param gmst The Menu gamestate (example: "main_menu")
--- @param clr The default color for the menu entries (optional)
+--- Creates a new Menu.
+-- @param gmst The Menu gamestate (example: "main_menu").
+-- @param clr The default color for the menu entries (optional).
 function Menu:new(gmst, clr)
     if clr == nil then
         clr = {81, 185, 141} -- default color
@@ -22,7 +28,7 @@ function Menu:new(gmst, clr)
     self.gamestate = gmst
 end
 
---- Updates the Menu object
+--- Updates the Menu object. Add this on your main love:update().
 function Menu:update()
     for k, v in pairs(entries) do
         local over = false
@@ -41,17 +47,18 @@ function Menu:update()
     end
 end
 
---- Updates the Menu object's label (internal use)
+--- Updates the Menu object's label.
+-- @param action Action to be updated
+-- @param label New label to set
 function Menu:updateLabel(action, label)
     for k, v in pairs(entries) do
-        print(v.action, v.label)
         if v.action == action then
             v.label = label
         end
     end
 end
 
---- Draw the Menu
+--- Draw the Menu. Add this on your main love.draw().
 function Menu:draw()
     local oldFont = love.graphics.getFont()
     local r,g,b,a = love.graphics.getColor()
@@ -70,14 +77,14 @@ function Menu:draw()
     love.graphics.setColor(r,g,b,a)
 end
 
---- Add a new Entry to the Menu
--- @param x number The X position of the entry
--- @param y number The Y position of the entry
--- @param w number The width of the entry
--- @param x number The height of the entry
--- @param x bool An Entry object could be clickable or not
--- @param onClick string Action to launch when you click this (default nil)
--- @param text string The Entry label
+--- Add a new Entry to the Menu.
+-- @param x The X position of the entry.
+-- @param y The Y position of the entry.
+-- @param w The width of the entry.
+-- @param h The height of the entry.
+-- @param clickable An Entry object could be clickable or not.
+-- @param onClick Action to launch when you click this (default nil).
+-- @param text The Entry label.
 function Menu:addEntry(x, y, w, h, clickable, onClick, text)
     local entry = {
         x = x,
@@ -93,7 +100,6 @@ function Menu:addEntry(x, y, w, h, clickable, onClick, text)
     table.insert(entries, entry)
 end
 
---- Mouse click event listener
 function love.mousereleased(x, y, m_button)
     if m_button == 1 then
         for k, v in pairs(entries) do
