c92270e4c696a453dfe9959a7529af6a0a54ab60
Juanjo Salvador <juanjosalvador@netc.eu>
2020-04-21 20:09:55 +0200

    Refactor
    Removed unnecesary for loops, removed the MenuController, implemented actions per button.

--- horchata/menu.lua
+++ horchata/menu.lua
@@ -1,15 +1,11 @@
---- Creates beautiful menus for your Love2D games
----- @module horchata.menu
----- @author Juanjo Salvador
----- @copyright 2020
----- @license MIT
-
 local Menu = {}
 
 local Color = require "lib.horchata.color"
+local Music = require 'lib.horchata.music'
 
 local entries = {}
 local screenwidth, screenheight, percent_x, percent_y, color, sound
+local highlighted, old_highlighted  = nil
 
 --- Creates a new Menu
 -- @param gamestate The Menu GAMESTATE (example: "main_menu")
@@ -18,41 +14,56 @@ local screenwidth, screenheight, percent_x, percent_y, color, sound
 function Menu:new(gamestate, color, sound)
     color = color or {81, 185, 141} -- default color
 
+    if sound ~= nil then
+        self.sound = sound
+    end
+
     screenwidth = love.graphics.getWidth()
     screenheight = love.graphics.getHeight()
     percent_x = screenwidth / 100
     percent_y = screenheight / 100
 
     self.color = Color:RGBtoLove(color)
+    self.sound:setLooping(false)
     self.GAMESTATE = gamestate
+end
+
+--- Refresh screen size values (if screen resizing)
+function Menu:refresh()
+    screenwidth = love.graphics.getWidth()
+    screenheight = love.graphics.getHeight()
+    percent_x = screenwidth / 100
+    percent_y = screenheight / 100
 end
 
 --- Updates the Menu object
 function Menu:update()
-    for k, v in pairs(entries) do
+    for _, entry in pairs(entries) do
         local mouse_x = love.mouse.getX()
         local mouse_y = love.mouse.getY()
-    
-        if mouse_y >= v.y * percent_y and mouse_y <= v.y * percent_y + v.h then
-            if mouse_x >= v.x * percent_x and mouse_x <= v.x * percent_x + v.w then
-                v.over = true
+
+        if mouse_y >= entry.y * percent_y and mouse_y <= entry.y * percent_y + entry.h then
+            if mouse_x >= entry.x * percent_x and mouse_x <= entry.x * percent_x + entry.w then
+                entry.m_over = true
             else
-                v.over = false
+                entry.m_over = false
             end
         else
-           v.over = false
+            entry.m_over = false
         end
     end
 end
 
---- Updates the Menu object's label (internal use)
-function Menu:updateLabel(action, label)
-    for k, v in pairs(entries) do
-        print(v.action, v.label)
-        if v.action == action then
-            v.label = label
-        end
-    end
+--- Updates the Menu object's label
+function Menu:updateLabel(x, y, w, h, enabled, label)
+    entries[label] = {
+        x = self.x,
+        y = self.y,
+        w = self.w,
+        h = self.h,
+        enabled = self.enabled,
+        label = self.label
+    }
 end
 
 --- Draw the Menu
@@ -61,7 +72,7 @@ function Menu:draw()
     local r,g,b,a = love.graphics.getColor()
     for k, v in pairs(entries) do
 
-        if v.over and v.clickable then
+        if v.m_over and v.enabled then
             love.graphics.setColor(self.color)
             love.graphics.print(v.label, v.x * percent_x, v.y * percent_y)
         else
@@ -75,38 +86,35 @@ function Menu:draw()
 end
 
 --- Add a new Entry to the Menu
--- @param x number The X position of the entry
--- @param y number The Y position of the entry
--- @param w number The width of the entry
--- @param h number The height of the entry
--- @param clickable bool An Entry object could be clickable or not
--- @param onClick string Action to launch when you click this (default nil)
--- @param text string The Entry label
-function Menu:addEntry(x, y, w, h, clickable, onClick, text)
-    local entry = {
+-- @param x The X position of the entry
+-- @param y The Y position of the entry
+-- @param w The width of the entry
+-- @param h The height of the entry
+-- @param enabled An Entry object can be enabled or not
+-- @param label The Entry label
+-- @param func The Entry function (whatever the function does)
+function Menu:addEntry(x, y, w, h, enabled, label, func)
+    entries[label] = {
         x = x,
         y = y,
         w = w,
         h = h,
-        mouse_over = false,
-        clickable = clickable,
-        action = onClick or nil,
-        label = text
+        enabled = enabled,
+        label = label,
+        func = func,
+        m_over = false
     }
-
-    table.insert(entries, entry)
 end
 
 -- Mouse click event listener
 function love.mousereleased(x, y, m_button)
     if m_button == 1 then
-        for k, v in pairs(entries) do
-            if v.over and v.clickable and GAMESTATE == Menu.GAMESTATE then
-                MenuController(v.action)
+        for _, entry in pairs(entries) do
+            if entry.m_over and entry.enabled and GAMESTATE == Menu.GAMESTATE then
+                entry:func()
             end
         end
     end
 end
-
 
 return Menu
